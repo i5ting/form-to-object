973ddf4720c4bee468041c400cf2413310ab8054
alfred sang <i5ting@126.com>
2015-06-01 20:07:38 +0800

    refact with no dependency && add test.html

--- index.js
+++ index.js
@@ -1,8 +1,30 @@
-/* jshint -W093 */
+var proto = Element.prototype;
+var vendor = proto.matches
+  || proto.matchesSelector
+  || proto.webkitMatchesSelector
+  || proto.mozMatchesSelector
+  || proto.msMatchesSelector
+  || proto.oMatchesSelector;
+ 
+/**
+ * Match `el` to `selector`.
+ *
+ * @param {Element} el
+ * @param {String} selector
+ * @return {Boolean}
+ * @api public
+ */
+  
+function matches(el, selector) {
+  if (vendor) return vendor.call(el, selector);
+  var nodes = el.parentNode.querySelectorAll(selector);
+  for (var i = 0; i < nodes.length; i++) {
+    if (nodes[i] == el) return true;
+  }
+  return false;
+}
 
-var matches = require('matches-selector')
-
-module.exports = function (form) {
+function form2obj(form) {
   var body = Object.create(null)
 
   Array.prototype.slice.call(form.querySelectorAll('input, textarea, select'))
