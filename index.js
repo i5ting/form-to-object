4817708075f9ffc0cc4f20e4c4cb534da9adf75c
alfred sang <i5ting@126.com>
2015-06-01 21:07:41 +0800

    bump to v2.0.1

--- index.js
+++ index.js
@@ -1,31 +1,36 @@
-var proto = Element.prototype;
-var vendor = proto.matches
-  || proto.matchesSelector
-  || proto.webkitMatchesSelector
-  || proto.mozMatchesSelector
-  || proto.msMatchesSelector
-  || proto.oMatchesSelector;
- 
-/**
- * Match `el` to `selector`.
- *
- * @param {Element} el
- * @param {String} selector
- * @return {Boolean}
- * @api public
+/*!
+ * form2obj v2.0.1 - index.js 
+ * Copyright(c) 2015 i5ting <shiren1118@126.com>
+ * MIT Licensed
  */
-  
-function matches(el, selector) {
-  if (vendor) return vendor.call(el, selector);
-  var nodes = el.parentNode.querySelectorAll(selector);
-  for (var i = 0; i < nodes.length; i++) {
-    if (nodes[i] == el) return true;
-  }
-  return false;
-}
 
 function form2obj(form) {
-  var body = Object.create(null)
+  var body = Object.create(null);
+  var proto = Element.prototype;
+  var vendor = proto.matches
+    || proto.matchesSelector
+    || proto.webkitMatchesSelector
+    || proto.mozMatchesSelector
+    || proto.msMatchesSelector
+    || proto.oMatchesSelector;
+ 
+  /**
+   * Match `el` to `selector`.
+   *
+   * @param {Element} el
+   * @param {String} selector
+   * @return {Boolean}
+   * @api public
+   */
+  
+  function matches(el, selector) {
+    if (vendor) return vendor.call(el, selector);
+    var nodes = el.parentNode.querySelectorAll(selector);
+    for (var i = 0; i < nodes.length; i++) {
+      if (nodes[i] == el) return true;
+    }
+    return false;
+  }
 
   Array.prototype.slice.call(form.querySelectorAll('input, textarea, select'))
   .forEach(function (el) {
